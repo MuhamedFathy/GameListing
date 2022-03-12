ad1a23cba147bbe6759574bf367f014b7c216df4
Mohamed Fathy <muhamed.gendy@gmail.com>
2022-03-12 10:03:43 +0200

    Add initial UI

--- presentation/src/main/java/com/github/gamelisting/MainActivity.kt
+++ presentation/src/main/java/com/github/gamelisting/MainActivity.kt
@@ -2,7 +2,11 @@ package com.github.gamelisting
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
 import com.github.gamelisting.databinding.ActivityMainBinding
 
 /**
@@ -16,5 +20,15 @@ class MainActivity : AppCompatActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContentView(ActivityMainBinding.inflate(layoutInflater).root)
+
+    val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment_game_details) as NavHostFragment
+    val navController = navHostFragment.navController
+    appBarConfiguration = AppBarConfiguration(navController.graph)
+    setupActionBarWithNavController(navController, appBarConfiguration)
+  }
+
+  override fun onSupportNavigateUp(): Boolean {
+    val navController = findNavController(R.id.nav_host_fragment_game_details)
+    return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
   }
 }
