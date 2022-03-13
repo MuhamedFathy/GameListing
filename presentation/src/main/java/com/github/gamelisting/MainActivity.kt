794a8f4a2db6ac3d10cc2847bbe5d788dd37b50b
Mohamed Fathy <muhamed.gendy@gmail.com>
2022-03-14 01:21:55 +0200

    Handling deleviring data to UI

--- presentation/src/main/java/com/github/gamelisting/MainActivity.kt
+++ presentation/src/main/java/com/github/gamelisting/MainActivity.kt
@@ -2,11 +2,6 @@ package com.github.gamelisting
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.findNavController
-import androidx.navigation.fragment.NavHostFragment
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.navigateUp
-import androidx.navigation.ui.setupActionBarWithNavController
 import com.github.gamelisting.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -17,20 +12,20 @@ import dagger.hilt.android.AndroidEntryPoint
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
-  private lateinit var appBarConfiguration: AppBarConfiguration
+//  private lateinit var appBarConfiguration: AppBarConfiguration
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContentView(ActivityMainBinding.inflate(layoutInflater).root)
 
-    val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment_game_details) as NavHostFragment
-    val navController = navHostFragment.navController
-    appBarConfiguration = AppBarConfiguration(navController.graph)
-    setupActionBarWithNavController(navController, appBarConfiguration)
+//    val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment_game_details) as NavHostFragment
+//    val navController = navHostFragment.navController
+//    appBarConfiguration = AppBarConfiguration(navController.graph)
+//    setupActionBarWithNavController(navController, appBarConfiguration)
   }
 
-  override fun onSupportNavigateUp(): Boolean {
-    val navController = findNavController(R.id.nav_host_fragment_game_details)
-    return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
-  }
+//  override fun onSupportNavigateUp(): Boolean {
+//    val navController = findNavController(R.id.nav_host_fragment_game_details)
+//    return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
+//  }
 }
---
+++ presentation/src/main/java/com/github/gamelisting/features/main/GamesAdapter.kt
@@ -0,0 +1,56 @@
+package com.github.gamelisting.features.main
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.paging.PagingDataAdapter
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.RecyclerView
+import com.github.gamelisting.databinding.ItemGameBinding
+import com.github.gamelisting.extension.load
+import com.github.gamelisting.features.main.GamesAdapter.GameViewHolder
+import com.github.gamelisting.features.main.viewmodel.uimodel.GameUIModel
+import javax.inject.Inject
+
+/**
+ * Authored by Mohamed Fathy on 13 Mar, 2022.
+ * Contact: muhamed.gendy@gmail.com
+ */
+class GamesAdapter @Inject constructor() : PagingDataAdapter<GameUIModel, GameViewHolder>(COMPARATOR) {
+
+  var itemCallback: ((Long) -> Unit)? = null
+
+  override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): GameViewHolder {
+    return GameViewHolder(ItemGameBinding.inflate(LayoutInflater.from(parent.context), parent, false))
+  }
+
+  override fun onBindViewHolder(holder: GameViewHolder, position: Int) {
+    getItem(position)?.let {
+      holder.bind(it)
+    }
+  }
+
+  companion object {
+
+    private val COMPARATOR = object : DiffUtil.ItemCallback<GameUIModel>() {
+      override fun areItemsTheSame(oldItem: GameUIModel, newItem: GameUIModel): Boolean {
+        return oldItem.id == newItem.id
+      }
+
+      override fun areContentsTheSame(oldItem: GameUIModel, newItem: GameUIModel): Boolean {
+        return oldItem == newItem
+      }
+    }
+  }
+
+  inner class GameViewHolder(private val binding: ItemGameBinding) : RecyclerView.ViewHolder(binding.root) {
+
+    fun bind(game: GameUIModel) {
+      with(game) {
+        binding.itemImageNameTextView.text = name
+        binding.itemImageRatingTextView.text = rating
+        binding.itemImagePosterImageView.load(poster)
+        binding.root.setOnClickListener { itemCallback?.invoke(id) }
+      }
+    }
+  }
+}
